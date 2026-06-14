5e8754c977b270b0defe2c2dd68ed72523e1fb20
KarkiChetan <karkichtn@gmail.com>
2026-06-15 01:22:17 +0530

    Create Leet-Code-234.cpp
    Palindrome-Linked-List

---
+++ 13-JUN-SAT-2026/Leet-Code-234.cpp
@@ -0,0 +1,53 @@
+https://leetcode.com/problems/palindrome-linked-list/submissions/2033196785/
+
+
+
+class Solution {
+public:
+
+    ListNode* reverse(ListNode* head) {
+
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+
+        while (curr != NULL) {
+
+            ListNode* next = curr->next;
+
+            curr->next = prev;
+
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    bool isPalindrome(ListNode* head) {
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != NULL && fast->next != NULL) {
+
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* secondHalf = reverse(slow);
+
+        ListNode* firstHalf = head;
+
+        while (secondHalf != NULL) {
+
+            if (firstHalf->val != secondHalf->val) {
+                return false;
+            }
+
+            firstHalf = firstHalf->next;
+            secondHalf = secondHalf->next;
+        }
+
+        return true;
+    }
+};
