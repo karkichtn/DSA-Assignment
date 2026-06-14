8624ec1f300a15bff983b0d6bbef07a58bc746e9
KarkiChetan <karkichtn@gmail.com>
2026-06-15 01:27:21 +0530

    Update Leet-Code-234.cpp
    Palindrome-Linked-List

--- 13-JUN-SAT-2026/Leet-Code-234.cpp
+++ 13-JUN-SAT-2026/Leet-Code-234.cpp
@@ -1,6 +1,6 @@
 https://leetcode.com/problems/palindrome-linked-list/submissions/2033196785/
 
-
+//Time = O(n), Space = O(1)
 
 class Solution {
 public:
@@ -46,6 +46,47 @@ public:
 
             firstHalf = firstHalf->next;
             secondHalf = secondHalf->next;
+        }
+
+        return true;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+//easy code
+//Time = O(n), Space =O(n)
+
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+
+        vector<int> v;
+
+        while(head != NULL) {
+            v.push_back(head->val);
+            head = head->next;
+        }
+
+        int left = 0;
+        int right = v.size() - 1;
+
+        while(left < right) {
+
+            if(v[left] != v[right]) {
+                return false;
+            }
+
+            left++;
+            right--;
         }
 
         return true;
